d603fea1bda2d3e69562de164fc9a2c0cd1daa74
Kaptenfilling <62646887+Kaptenfilling@users.noreply.github.com>
2021-09-21 11:52:47 +0200

    lagt till alla bilder

--- Python_test_gui.py
+++ Python_test_gui.py
@@ -15,24 +15,35 @@ def get_img_data(f, maxsize=(480, 800)):
 
 sg.theme('Black')
 
-chargerID = get_img_data('Pictures/chargerid.png')
-startingUp = get_img_data('Pictures/StartingUp.png')
-notAvailable = get_img_data('Pictures/NotAvailable.png')
+img_chargerID = get_img_data('Pictures/chargerid.png')
+img_startingUp = get_img_data('Pictures/StartingUp.png')
+img_notAvailable = get_img_data('Pictures/NotAvailable.png')
+img_errorWhileCharging = get_img_data('Pictures/AnErrorOccuredWhileCharging.png')
+img_authorizing = get_img_data('Pictures/authorizing.png')
+img_charging = get_img_data('Pictures/charging.png')
+img_chargingCancelled = get_img_data('Pictures/ChargingCancelled.png')
+img_connectingToCar = get_img_data('Pictures/ConnectingToCar.png')
+img_disconnectingFromCar = get_img_data('Pictures/DisconnectingFromCar.png')
+img_followInstructions = get_img_data('Pictures/FollowInstructions.png')
+img_fullyCharged = get_img_data('Pictures/FullyCharged.png')
+img_plugInCable = get_img_data('Pictures/PlugInCable.png')
+img_rfidNotValid = get_img_data('Pictures/RFIDnotValid.png')
+img_unableToCharge = get_img_data('Pictures/UnableToCharge.png')
 
 layout1 =    [
-                [sg.Image(data=startingUp, key='__IMAGE__', size=(480, 800))]
+                [sg.Image(data=img_startingUp, key='__IMAGE__', size=(480, 800))]
             ]
 
 window = sg.Window(title="FlexiCharge", layout=layout1, no_titlebar=True, location=(0,0), size=(480,800), keep_on_top=False).Finalize()
 #window.Maximize()
 
-test = 0
+screen = 0
 while True:
-    if test == 2:
-        test = 0
-        window['__IMAGE__'].update(data=chargerID)
+    if screen == 2:
+        screen = 0
+        window['__IMAGE__'].update(data=img_chargerID)
         window.refresh()
     else:
-        test += 1
+        screen += 1
         time.sleep(3)
 window.close()
