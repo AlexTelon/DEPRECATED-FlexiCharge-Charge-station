469807e3e7d265a3b434d19552e1f93e9e401fee
JonathanLuks <62608070+JonathanLuks@users.noreply.github.com>
2021-10-05 10:42:07 +0200

    Create test.py

---
+++ Test-folder/test.py
@@ -0,0 +1,159 @@
+import asyncio
+import websockets
+import json
+import time
+import nest_asyncio
+
+# from apscheduler.schedulers.background import BackgroundScheduler
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
+from datetime import datetime
+
+
+from ocpp.routing import on
+from ocpp.v16 import ChargePoint as cp
+from ocpp.v16.enums import Action, RegistrationStatus
+from ocpp.v16 import call_result, call
+
+
+from StateHandler import States
+from StateHandler import StateHandler
+
+loop = asyncio.get_event_loop()
+
+async def send_heartbeat(ws):
+    while True:
+        hb = [2, "123abc", "Heartbeat", {}]
+        z = json.dumps(hb)
+        print("Sending Heartbeat...")
+        await ws.send(z)
+        print(await ws.recv())
+        await asyncio.sleep(2)
+
+
+async def connect():
+    url = "ws://54.220.194.65:1337/123abc"
+    async with websockets.connect(url, ping_interval=None, timeout=None) as websocket:
+        print("Connected.")
+
+        pkg = [2, "0jdsEnnyo2kpCP8FLfHlNpbvQXosR5ZNlh8v", "BootNotification", {
+            "chargePointVendor": "AVT-Company",
+            "chargePointModel": "AVT-Express",
+            "chargePointSerialNumber": "avt.001.13.1",
+            "chargeBoxSerialNumber": "avt.001.13.1.01",
+            "firmwareVersion": "0.9.87",
+            "iccid": "",
+            "imsi": "",
+            "meterType": "AVT NQC-ACDC",
+            "meterSerialNumber": "avt.001.13.1.01" } ]
+        pkg_send = json.dumps(pkg)
+        await websocket.send(pkg_send)
+        resp = await websocket.recv()
+        resp_parsed = json.loads(resp)
+        chargerID = resp_parsed[2]['chargerID']
+        l = list(str(chargerID))
+        print(l)
+        #print(chargerID)
+
+        x = [2, "knaskalas", "Authorize", {"idTag": "B4A63CDF"}]
+        y = json.dumps(x)
+        await websocket.send(y)
+        print("Response: " + await websocket.recv())
+
+        # sched = BackgroundScheduler()
+        # sched.add_job(lambda: send_heartbeat(websocket), 'interval', seconds=5)
+        # sched.start()
+
+        x = [2, "knaskalas", "StartTransaction", {
+            "connectorId": 2,
+            "idTag": "B4A63CDF",
+            "timestamp": datetime.today().strftime('%Y-%m-%d-%H:%M:%S'),
+            "meterStart": 1,
+            "reservationId": 0
+        }]
+        y = json.dumps(x)
+        print("Sending transaction request...")
+        await websocket.send(y)
+        resp = await websocket.recv()
+        print("Response: " + resp)
+        resp_parsed = json.loads(resp)
+        # print(resp_parsed[2]['transactionId'])
+
+        x = [2, "knaskalas", "StopTransaction", {
+            "transactionId": resp_parsed[2]['transactionId'],
+            "idTag": "B4A63CDF",
+            "timestamp": datetime.today().strftime('%Y-%m-%d-%H:%M:%S'),
+            "meterStop": 1
+        }]
+        y = json.dumps(x)
+        print("Stopping transaction...")
+        await websocket.send(y)
+        print("Response: " + await websocket.recv())
+
+        #while True:
+        #    hb = [2, "knaskalas", "Heartbeat", {}]
+        #    z = json.dumps(hb)
+        #    print("Sending Heartbeat...")
+        #    await websocket.send(z)
+        #    print("Response: " + await websocket.recv())
+        #    time.sleep(3)
+
+
+
+async def statemachine():
+    while True:
+
+        if state.get_state() == States.S_STARTUP:
+            print("Starting up...")
+
+            # Only for temporary testing purposes:
+            # await connect()
+
+            state.set_state(States.S_AVAILABLE)
+            # Pseudo-code:
+            # if charger_connected:
+            #    state.set_state(States.S_CONNECTED)
+            # else
+            #    state.set_state(States.S_NOTAVAILABLE)
+            ####################
+
+        elif state.get_state() == States.S_AVAILABLE:
+            print("statemachine!!")
+
+            await asyncio.sleep(4)
+
+        elif state.get_state() == States.S_NOTAVAILABLE:
+            pass
+
+        elif state.get_state() == States.S_CONNECTING:
+            pass
+
+        elif state.get_state() == States.S_CONNECTED:
+            pass
+
+        elif state.get_state() == States.S_DISPLAYID:
+            pass
+
+        elif state.get_state() == States.S_AUTHORIZING:
+            pass
+
+        elif state.get_state() == States.S_PLUGINCABLE:
+            pass
+
+        else:
+            print("wtf man.")
+
+
+async def main():
+    url = "ws://localhost:9000/knaskalas"
+    global loop
+    async with websockets.connect(url, ping_interval=None, timeout=None) as websocket:
+        tasks = [
+            loop.create_task(statemachine()),
+            loop.create_task(send_heartbeat(websocket)),
+        ]
+        loop.run_until_complete(asyncio.wait(tasks))
+
+nest_asyncio.apply()
+
+state = StateHandler()
+loop.run_until_complete(main())
