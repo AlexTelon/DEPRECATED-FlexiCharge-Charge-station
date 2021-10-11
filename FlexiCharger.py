a6f62e5f24247249d399f028989a6182b3583c43
Kaptenfilling <62646887+Kaptenfilling@users.noreply.github.com>
2021-10-11 15:52:10 +0200

    datatransfer

--- FlexiCharger.py
+++ FlexiCharger.py
@@ -198,8 +198,15 @@ async def main():
             resp = await websocket.recv()
             resp_parsed = json.loads(resp)
             print(resp_parsed)
-            print(resp_parsed[3]['chargerId'])
-            temp = resp_parsed[3]['chargerId']
+            resp2 = await websocket.recv()
+            resp_parsed2 = json.loads(resp2)
+            print(resp_parsed2)
+            for a in resp_parsed2:
+                print(type(a))
+            nisse = json.loads(resp_parsed2[3]['data'])
+            print(nisse["chargerId"])
+            print(nisse["chargerId"])
+            temp = nisse["chargerId"]
             chargerID = list(str(temp))
             tasks = [
                 asyncio.get_event_loop().create_task(statemachine(websocket)),
